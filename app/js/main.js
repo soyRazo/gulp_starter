92569bf396dc6e2db9c8e1307dbe84ce4cd737ff
Carlos Razo <c.razo.a@gmail.com>
2015-11-18 18:50:29 -0600

    agregar main.js

---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+console.log("Great job! Now get back to work");
+console.log("And other stuff here");
+console.log("And more stuff");
+console.log("happy happy");
+
+document.getElementById('sweet-header').innerHTML = 'AMAZING-SPIDERMANiakas';
